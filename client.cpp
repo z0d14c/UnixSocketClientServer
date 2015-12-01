33e03c2bf4ef2210752b3dc3a80f28ab3243307d
thomas grice <augustus.grice@gmail.com>
2015-12-01 05:30:41 -0500

    init commit

---
+++ client.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#define UNIX_PATH_MAX    108
+
+using namespace std;
+
+int main(int argc, char* args[]){
+	struct sockaddr_un address;
+	int  socket_fd, nbytes;
+	char buffer[256];
+	cout<<"Project 3 client started."<<endl;
+
+	socket_fd = socket(PF_UNIX, SOCK_STREAM, 0);
+	 if(socket_fd < 0)
+	 {
+	  printf("socket() failed\n");
+	  return 1;
+	 }
+
+	 /* start with a clean address structure */
+	 memset(&address, 0, sizeof(struct sockaddr_un));
+	 
+	 address.sun_family = AF_UNIX;
+	 snprintf(address.sun_path, 100, "./demo_socket");
+
+	 if(connect(socket_fd, 
+	            (struct sockaddr *) &address, 
+	            sizeof(struct sockaddr_un)) != 0)
+	 {
+	  printf("connect() failed\n");
+	  return 1;
+	 }
+
+	 nbytes = snprintf(buffer, 256, "hello from a client");
+	 write(socket_fd, buffer, nbytes);
+	 
+	 nbytes = read(socket_fd, buffer, 256);
+	 buffer[nbytes] = 0;
+
+	 printf("MESSAGE FROM SERVER: %s\n", buffer);
+
+	 close(socket_fd);
+
+	 return 0;
+}
