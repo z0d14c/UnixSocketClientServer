0e7b51d24bdbf9efe62f3e390ebebf831f69c5bc
thomas grice <augustus.grice@gmail.com>
2015-12-05 18:17:33 -0500

    new files

--- client.cpp
+++ client.cpp
@@ -10,60 +10,118 @@
 #define UNIX_PATH_MAX    108
 
 using namespace std;
-//display menu to da user
-char menu() {
-	string input = "";
-	cout<<"CLIENT MENU:"<<endl;
-	cout<<"1. Option 1"<<endl;
-	cout<<"2. Option 2"<<endl;
-	cout<<"3. Option 3"<<endl;
-	cout<<"4. Option 4"<<endl;
-	cout<<"0. Exit"<<endl;
-	getline(cin, input);
-	return input[0];
+
+void listFiles(string input) {
+  int delimLoc = input.find("!!!");
+  int endDelimLoc = input.find("###");
+  int initLoc = 0;
+  int numFiles = 0;
+  cout << "**** LIST OF FILES ****" << endl;
+  while (delimLoc != endDelimLoc - 3) {
+    numFiles++;
+    string fileName = input.substr(initLoc, delimLoc);
+    delimLoc = input.find("!!!", initLoc);
+    initLoc = delimLoc + 3;
+    cout << numFiles << ". " << fileName << endl;
+  }
 }
 
-int main(int argc, char* args[]){
-	struct sockaddr_un address; //sockaddr_un means a unix domain socket (instead of internet domain)
-	int  socket_fd, nbytes;
-	cout<<"Project 3 client started."<<endl;
+//interprets server response and outputs relevant info
+string interpret(char command, string input) {
+  switch (command) {
+  case '1':
+    listFiles(input);
+    break;
+  case '2':
 
-	//SOCK_STREAM is socket communication type, SOCK_STREAM two-way, reliable connection
-	socket_fd = socket(PF_UNIX, SOCK_STREAM, 0); // arg1 is communication domain, PF_UNIX is domain for local communication
+    break;
+  case '3':
+    break;
+  case '4':
+    break;
+  case '5':
+    break;
+  default:
+    break;
+  }
+  return "lol";
+}
 
-	 if(socket_fd < 0) //check for socket opening failure
-	 {
-	  printf("socket() failed\n");
-	  return 1;
-	 }
+//display menu to da user
+char menu() {
+  string input = "";
+  cout << "CLIENT MENU:" << endl;
+  cout << "1. Display names of files" << endl;
+  cout << "2. Get a file" << endl;
+  cout << "3. Remove a file" << endl;
+  cout << "4. Add a new file" << endl;
+  cout << "0. Exit" << endl;
+  getline(cin, input);
+  return input[0];
+}
 
-	 memset(&address, 0, sizeof(struct sockaddr_un)); //sets up clean memory
-	 
-	 address.sun_family = AF_UNIX; //set sun_family to AF_UNIX 
-	 snprintf(address.sun_path, 100, "./demo_socket");
+//allows user to add parameters, if applicable, to the command
+string processMenuChoice(char command) {
+  switch (command) {
+  case '1':
+    return "1";
+    break;
+  case '2':
 
-	 if(connect(socket_fd, 
-	            (struct sockaddr *) &address, 
-	            sizeof(struct sockaddr_un)) != 0)
-	 {
-	  printf("connect() failed\n");
-	  return 1;
-	 }
-	char choice = '0'; // initialize to 0
-	while(choice != '5') {
-		choice = menu();
-		char buffer[256];
-		 nbytes = snprintf(buffer, 256, "%c", choice);
-		 write(socket_fd, buffer, nbytes);
-		 
-		 nbytes = read(socket_fd, buffer, 256); //reads, tells it to wait until 256 bytes have been read
-		 buffer[nbytes] = 0;
+    break;
+  case '3':
+    break;
+  case '4':
+    break;
+  case '5':
+    break;
+  default:
+    break;
+  }
+}
 
-		 printf("MESSAGE FROM SERVER: %s\n", buffer);
-	 }
-	 close(socket_fd); //close socket connection
-	 cout<<"socket fd "<<socket_fd<<endl;
+int main(int argc, char* args[]) {
+  struct sockaddr_un address; //sockaddr_un means a unix domain socket (instead of internet domain)
+  int  socket_fd, nbytes;
+  cout << "Project 3 client started." << endl;
 
-	 return 0;
+  //SOCK_STREAM is socket communication type, SOCK_STREAM two-way, reliable connection
+  socket_fd = socket(PF_UNIX, SOCK_STREAM, 0); // arg1 is communication domain, PF_UNIX is domain for local communication
+
+  if (socket_fd < 0) //check for socket opening failure
+  {
+    printf("socket() failed\n");
+    return 1;
+  }
+
+  memset(&address, 0, sizeof(struct sockaddr_un)); //sets up clean memory
+
+  address.sun_family = AF_UNIX; //set sun_family to AF_UNIX
+  snprintf(address.sun_path, 100, "./demo_socket");
+
+  if (connect(socket_fd,
+              (struct sockaddr *) &address,
+              sizeof(struct sockaddr_un)) != 0)
+  {
+    printf("connect() failed\n");
+    return 1;
+  }
+  char choice = '0'; // initialize to 0
+  while (choice != '5') {
+    choice = menu();
+    char buffer[256];
+    nbytes = snprintf(buffer, 256, "%c", choice);
+    write(socket_fd, buffer, nbytes);
+
+    nbytes = read(socket_fd, buffer, 256); //reads, tells it to wait until 256 bytes have been read
+    interpret(choice, buffer);
+    buffer[nbytes] = 0;
+
+    //printf("MESSAGE FROM SERVER: %s\n", buffer);
+  }
+  close(socket_fd); //close socket connection
+  cout << "socket fd " << socket_fd << endl;
+
+  return 0;
 }
 
--- server.cpp
+++ server.cpp
@@ -1,92 +1,161 @@
-#include <iostream>
+#include <iostream> //input/output stream
 #include <iomanip>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <dirent.h>
 #include <sys/un.h>
 #include <stdio.h>
+#include <fstream>  //write to file
+#include <sstream> //write to memory (string)
 #include <string.h>
 #include <unistd.h>
+#include <vector>
+#define GetCurrentDir getcwd
 #define UNIX_PATH_MAX    108
-
 using namespace std;
+char currentPath[FILENAME_MAX];
+char filesPath[FILENAME_MAX];
+vector<string> files;
 
+void readDirectory() {
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir (filesPath)) != NULL) {
+    /* print all the files and directories within directory */
+    while ((ent = readdir (dir)) != NULL) {
+      if (ent->d_name[0] != '.') {
+        files.push_back(ent->d_name);
+      }
+    }
+    closedir (dir);
+  }
+}
+
+//return file list for display command
+//!!! is delimeter between files
+//### is end of message delimiter
+string listFiles() {
+  string filesList = "";
+  for(int i=0; i<files.size(); i++){
+    filesList = filesList + files.at(i) + "!!!";
+  }
+  filesList = filesList + "###";
+  return filesList;
+}
+
+//reads files in initially
+void initFiles() {
+  cout << "reading files in files/ dir" << endl;
+  GetCurrentDir(currentPath, sizeof(currentPath));
+  snprintf(filesPath, sizeof(filesPath), "%s%s", currentPath, "/files");
+  readDirectory();
+}
+
+//translate client command
+//1 display
+//2 get file
+//3 remove file
+//4 add new file
+//pattern is [0] = command [1 to indexOf("###")] = parameters
+// returns '1' for success, '0' for failure, or a string of delimited files for display
+string execute(char* command) {
+  switch(command[0]){
+    case '1':
+      return listFiles();
+      break;
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+      return "1";
+      break;
+    default:
+      return "0";
+      break;
+  }
+}
+
+//handles the connection, does the high-level stuff
+//buffer[0] is command, rest is parameters/delimiters/empty
 int connection_handler(int connection_fd)
 {
-	int nbytes;
-	 char buffer[256];
- 	 char choice = '0';
-	 while(choice != '5'){
-	// while(nbytes = read(connection_fd, buffer, 256) != 0){
-	 nbytes = read(connection_fd, buffer, 256); //reads, tells it to wait until 256 bytes have been read
-	 	cout<<"server side buffer"<<endl;
-	 	choice = buffer[0];
-	 	cout<<"user choice is "<<choice<<endl;
-		 buffer[nbytes] = 0; // (start after read bytes) to be sent to client
+  int nbytes;
+  char buffer[256];
+  buffer[0] = '0'; //0 means user hasn't chosen yet! aka connection just started
+  string response;
+  while (buffer[0] != '5') {
+    // while(nbytes = read(connection_fd, buffer, 256) != 0){
+    nbytes = read(connection_fd, buffer, 256); //reads, tells it to wait until 256 bytes have been read
+    response = execute(buffer);
+    buffer[nbytes] = 0; // (start after read bytes) to be sent to client
 
-		 printf("MESSAGE FROM CLIENT: %s\n", buffer); //read from client
-		 nbytes = snprintf(buffer, 256, "hello from the server"); //print to buffer (send to client)
-		 write(connection_fd, buffer, nbytes);
- 	// }
-		}
-	cout<<"exiting connection handler"<<endl;
- close(connection_fd);
- return 0;
+    //printf("MESSAGE FROM CLIENT: %s\n", buffer); //read from client
+    nbytes = snprintf(buffer, 256, "%s", response.c_str()); //print to buffer (send to client)
+    write(connection_fd, buffer, nbytes);
+    // }
+  }
+  cout << "exiting connection handler" << endl;
+  close(connection_fd);
+  return 0;
 }
 
 int main(void)
 {
- struct sockaddr_un address;
- int socket_fd, connection_fd;
- socklen_t address_length;
- pid_t child;
- cout<<"Project 3 server starting."<<endl;
+//read files
+  initFiles();
+//set up socket stuff
+  struct sockaddr_un address;
+  int socket_fd, connection_fd;
+  socklen_t address_length;
+  pid_t child;
+  cout << "Project 3 server starting." << endl;
 
- socket_fd = socket(PF_UNIX, SOCK_STREAM, 0);
- if(socket_fd < 0)
- {
-  printf("socket() failed\n");
-  return 1;
- } 
-
- unlink("./demo_socket"); //delete demo socket file
-
- /* start with a clean address structure */
- memset(&address, 0, sizeof(struct sockaddr_un));
-
- address.sun_family = AF_UNIX;
- snprintf(address.sun_path, UNIX_PATH_MAX, "./demo_socket"); 
-
- if(bind(socket_fd, 
-         (struct sockaddr *) &address, 
-         sizeof(struct sockaddr_un)) != 0)
- {
-  printf("bind() failed\n");
-  return 1;
- }
-
- if(listen(socket_fd, 5) != 0)
- {
-  printf("listen() failed\n");
-  return 1;
- }
-
- while((connection_fd = accept(socket_fd, 
-                               (struct sockaddr *) &address,
-                               &address_length)) > -1)
- {
-  child = fork();
-  if(child == 0)
+  socket_fd = socket(PF_UNIX, SOCK_STREAM, 0);
+  if (socket_fd < 0)
   {
-   /* now inside newly created connection handling process */
-   connection_handler(connection_fd);
+    printf("socket() failed\n");
+    return 1;
   }
 
-  /* still inside server process */
-  //close(connection_fd);
- }
+  unlink("./demo_socket"); //delete demo socket file
 
- close(socket_fd);
- unlink("./demo_socket");
- return 0;
+  /* start with a clean address structure */
+  memset(&address, 0, sizeof(struct sockaddr_un));
+
+  address.sun_family = AF_UNIX;
+  snprintf(address.sun_path, UNIX_PATH_MAX, "./demo_socket");
+
+  if (bind(socket_fd,
+           (struct sockaddr *) &address,
+           sizeof(struct sockaddr_un)) != 0)
+  {
+    printf("bind() failed\n");
+    return 1;
+  }
+
+  if (listen(socket_fd, 5) != 0)
+  {
+    printf("listen() failed\n");
+    return 1;
+  }
+
+  while ((connection_fd = accept(socket_fd,
+                                 (struct sockaddr *) &address,
+                                 &address_length)) > -1)
+  {
+    child = fork();
+    if (child == 0)
+    {
+      /* now inside newly created connection handling process */
+      connection_handler(connection_fd);
+    }
+
+    /* still inside server process */
+    //close(connection_fd);
+  }
+
+  close(socket_fd);
+  unlink("./demo_socket");
+  return 0;
 }
