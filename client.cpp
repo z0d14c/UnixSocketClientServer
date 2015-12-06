356ac60729473b1eadfe02488c8f9f009fc77ab7
thomas grice <augustus.grice@gmail.com>
2015-12-05 20:23:30 -0500

    add/remove file fn

--- client.cpp
+++ client.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
@@ -11,35 +12,51 @@
 
 using namespace std;
 
+//list files on server
 void listFiles(string input) {
   int delimLoc = input.find("!!!");
   int endDelimLoc = input.find("###");
   int initLoc = 0;
   int numFiles = 0;
   cout << "**** LIST OF FILES ****" << endl;
-  while (delimLoc != endDelimLoc - 3) {
-    numFiles++;
-    string fileName = input.substr(initLoc, delimLoc);
-    delimLoc = input.find("!!!", initLoc);
-    initLoc = delimLoc + 3;
-    cout << numFiles << ". " << fileName << endl;
+  if (endDelimLoc > 1) {
+    do {
+      numFiles++;
+      string fileName = input.substr(initLoc, delimLoc);
+      delimLoc = input.find("!!!", initLoc);
+      initLoc = delimLoc + 3;
+      cout << numFiles << ". " << fileName << endl;
+    } while (delimLoc != endDelimLoc - 3 && endDelimLoc > 1);
   }
+}
+
+void saveFile(string input){
+  int endDelimLoc = input.find("###");
+  int separatorLoc = input.find("!!!");
+  string fileName = input.substr(0, separatorLoc);
+  string fileContent = input.substr(separatorLoc+3, endDelimLoc);
+  ofstream outputFile(fileName.c_str());
+  outputFile << fileContent;
+  outputFile.close();
+
 }
 
 //interprets server response and outputs relevant info
 string interpret(char command, string input) {
+  cout<<"command "<<command<<endl;
+  cout<<"input "<<input<<endl;
   switch (command) {
-  case '1':
+  case '1': //list files
     listFiles(input);
     break;
-  case '2':
-
+  case '2': //getFile
+    saveFile(input);
     break;
-  case '3':
+  case '3': //remove file
     break;
   case '4':
     break;
-  case '5':
+  case '5': //quit
     break;
   default:
     break;
@@ -47,8 +64,50 @@ string interpret(char command, string input) {
   return "lol";
 }
 
+//accepts input from user on what to deletes
+char deleteFile() {
+  char fileSelection;
+  cout << "Please enter the file number you would like to delete" << endl;
+  cin >> fileSelection;
+  return fileSelection;
+}
+
+//accepts input from user on what to deletes
+char getFile() {
+  char fileSelection;
+  cout << "Please enter the file number you would like to get" << endl;
+  cin >> fileSelection;
+  return fileSelection;
+}
+
+//allows user to add parameters, if applicable, to the command
+string processMenuChoice(char command) {
+  string commandAndParam = "";
+  commandAndParam += command;
+  switch (command) {
+  case '1': //list files
+    return "1";
+    break;
+  case '2': //get file
+    commandAndParam += getFile();
+    return commandAndParam;
+    break;
+  case '3': //remove file
+    commandAndParam += deleteFile();
+    return commandAndParam;
+    break;
+  case '4': //add file
+    return "4";
+    break;
+  case '5': //exit
+    return "5";
+    break;
+  default:
+    break;
+  }
+}
 //display menu to da user
-char menu() {
+string menu() {
   string input = "";
   cout << "CLIENT MENU:" << endl;
   cout << "1. Display names of files" << endl;
@@ -56,29 +115,11 @@ char menu() {
   cout << "3. Remove a file" << endl;
   cout << "4. Add a new file" << endl;
   cout << "0. Exit" << endl;
-  getline(cin, input);
-  return input[0];
+  cin >> input;
+  return processMenuChoice(input[0]);
 }
 
-//allows user to add parameters, if applicable, to the command
-string processMenuChoice(char command) {
-  switch (command) {
-  case '1':
-    return "1";
-    break;
-  case '2':
 
-    break;
-  case '3':
-    break;
-  case '4':
-    break;
-  case '5':
-    break;
-  default:
-    break;
-  }
-}
 
 int main(int argc, char* args[]) {
   struct sockaddr_un address; //sockaddr_un means a unix domain socket (instead of internet domain)
@@ -90,7 +131,7 @@ int main(int argc, char* args[]) {
 
   if (socket_fd < 0) //check for socket opening failure
   {
-    printf("socket() failed\n");
+    printf("socket() no bueno\n");
     return 1;
   }
 
@@ -106,16 +147,19 @@ int main(int argc, char* args[]) {
     printf("connect() failed\n");
     return 1;
   }
-  char choice = '0'; // initialize to 0
-  while (choice != '5') {
-    choice = menu();
-    char buffer[256];
-    nbytes = snprintf(buffer, 256, "%c", choice);
-    write(socket_fd, buffer, nbytes);
-
+  char buffer[256];
+  buffer[0] = '0'; // initialize to 0
+  while (buffer[0] != '5') {
+    string choice = menu();
+    cout << "choice is " << choice << endl;
+    nbytes = snprintf(buffer, 256, "%s", choice.c_str()); // write into buffer
+    write(socket_fd, buffer, nbytes); //send across socket
+    //wait for response
     nbytes = read(socket_fd, buffer, 256); //reads, tells it to wait until 256 bytes have been read
-    interpret(choice, buffer);
-    buffer[nbytes] = 0;
+    string tempString = string(buffer);
+    int endDelimLoc = tempString.find("###");
+    string relevantInput = tempString.substr(0, endDelimLoc + 3);
+    interpret(choice[0], relevantInput);
 
     //printf("MESSAGE FROM SERVER: %s\n", buffer);
   }
--- server.cpp
+++ server.cpp
@@ -10,6 +10,7 @@
 #include <sstream> //write to memory (string)
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <vector>
 #define GetCurrentDir getcwd
 #define UNIX_PATH_MAX    108
@@ -17,7 +18,36 @@ using namespace std;
 char currentPath[FILENAME_MAX];
 char filesPath[FILENAME_MAX];
 vector<string> files;
+vector<string> fileContents;
 
+
+//adds a file to the virtual file system
+void addFile(string fileName, string fileData) {
+  files.push_back(fileName);
+  fileContents.push_back(fileData);
+}
+
+//return file content
+string getFile(string params){
+  int choiceInt = atoi(params.c_str());
+  string fileContent = fileContents.at(choiceInt);
+  string fileName = files.at(choiceInt);
+  fileContent = fileName + "!!!" + fileContent + "###";
+  return fileContent;
+}
+
+//remove file from virtual system
+void deleteFile(string choice) {
+  int choiceInt = atoi(choice.c_str());
+  choiceInt--;
+  cout<<"choice int is "<<choiceInt<<endl;
+  cout<<"files size before "<<files.size()<<endl;
+  files.erase(files.begin() + choiceInt);
+  fileContents.erase(fileContents.begin() + choiceInt);
+  cout<<"files size after "<<files.size()<<endl;
+}
+
+//read files from directory
 void readDirectory() {
   DIR *dir;
   struct dirent *ent;
@@ -25,7 +55,8 @@ void readDirectory() {
     /* print all the files and directories within directory */
     while ((ent = readdir (dir)) != NULL) {
       if (ent->d_name[0] != '.') {
-        files.push_back(ent->d_name);
+        string fileContent = "FILLERCONTENT";
+        addFile(ent->d_name, fileContent);
       }
     }
     closedir (dir);
@@ -37,10 +68,12 @@ void readDirectory() {
 //### is end of message delimiter
 string listFiles() {
   string filesList = "";
-  for(int i=0; i<files.size(); i++){
+  cout<<"files size in list files"<<files.size()<<endl;
+  for (int i = 0; i < files.size(); i++) {
     filesList = filesList + files.at(i) + "!!!";
   }
   filesList = filesList + "###";
+  cout<<"files list is "<<filesList<<endl;
   return filesList;
 }
 
@@ -60,19 +93,27 @@ void initFiles() {
 //pattern is [0] = command [1 to indexOf("###")] = parameters
 // returns '1' for success, '0' for failure, or a string of delimited files for display
 string execute(char* command) {
-  switch(command[0]){
-    case '1':
-      return listFiles();
-      break;
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-      return "1";
-      break;
-    default:
-      return "0";
-      break;
+  string commandString(command);
+  string params = commandString.substr(1, commandString.find("###"));
+  cout<<params<<endl;
+  switch (command[0]) {
+  case '1': //list files
+    return listFiles();
+    break;
+  case '2': //get file
+    return getFile(params);
+    break;
+  case '3': //delete file
+      deleteFile(params);
+    return "1###";
+    break;
+  case '4': //add file
+  case '5': //exit
+    return "1";
+    break;
+  default:
+    return "0";
+    break;
   }
 }
 
@@ -85,11 +126,13 @@ int connection_handler(int connection_fd)
   buffer[0] = '0'; //0 means user hasn't chosen yet! aka connection just started
   string response;
   while (buffer[0] != '5') {
+    cout<<"listening for response"<<endl;
     // while(nbytes = read(connection_fd, buffer, 256) != 0){
     nbytes = read(connection_fd, buffer, 256); //reads, tells it to wait until 256 bytes have been read
     response = execute(buffer);
     buffer[nbytes] = 0; // (start after read bytes) to be sent to client
-
+    cout<<"response lol "<<response<<endl;
+    cout<<"bufer lol "<<buffer<<endl;
     //printf("MESSAGE FROM CLIENT: %s\n", buffer); //read from client
     nbytes = snprintf(buffer, 256, "%s", response.c_str()); //print to buffer (send to client)
     write(connection_fd, buffer, nbytes);
